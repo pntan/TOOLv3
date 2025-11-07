f77153479bdc46db22b51f0b48e1e0a3543e8162
Tân <nhattanphan2014@gmail.com>
2025-11-07 16:23:30 +0700

    Update TOOL.js

--- TOOL.js
+++ TOOL.js
@@ -1,26 +1,20 @@
 // ==UserScript==
 // @name         CÔNG CỤ HỖ TRỢ V3
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=http://anonymouse.org/
-// @namespace    tnaphan.toolv3
-// @version      0.0.2a
+// @namespace    tanphan.toolv3
+// @version      0.0.3
 // @description  Một số công cụ hỗ trợ công việc
 // @author       Phan Nhật Tân
 // @match        *://*/*
 // @grant        none
 // @updateURL    https://cdn.jsdelivr.net/gh/pntan/TOOLv3@lastest/TOOL.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/pntan/TOOLv3@lastest/TOOL.js
-// @require      https://code.jquery.com/jquery-3.7.1.min.js
-// @require      https://code.jquery.com/ui/1.14.1/jquery-ui.js
-// @require      https://cdn.jsdelivr.net/npm/dompurify@2.4.0/dist/purify.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     // Code của bạn
-    const VERSION = '0.0.2a';
+    const VERSION = '0.0.3';
 
     console.log(`CÔNG CỤ HỖ TRỢ V3 - Phiên bản ${VERSION} đã được kích hoạt.`);
 })();
