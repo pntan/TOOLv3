31ace45e44356e1f63176c9bc39d39b4f7ac0526
pntan <nhattanphan2014@gmail.com>
2025-11-07 22:37:30 +0700

    Update TOOL.js

--- TOOL.js
+++ TOOL.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         CÔNG CỤ HỖ TRỢ V3
+// @name         CÔNG CỤ HỖ TRỢ V4
 // @version      0.0.1
 // @namespace    tanphan.toolv3
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=http://anonymouse.org/
@@ -16,6 +16,14 @@
 	// Code của bạn
 	const VERSION = '0.0.1';
 			
+ /**
+  * @func getUrlServer
+  * @description 'Lấy dữ liệu từ file GITHUB'
+  * @param owner = "pntan" 'params0'
+  * @param repo = "TOOLv3" 'params1'
+  * @param path = "version" 'params2'
+  * @param branch = "main" 'params3'
+  */
 	async function getUrlServer(owner = "pntan", repo = "TOOLv3", path = "version", branch = "main") {
 		try {
 		// Lấy file từ GitHub API
@@ -35,7 +43,12 @@
 		}
 	}
 
-	// Ghi console log
+ /**
+  * @func boxAlert
+  * @description 'Ghi console log với định dạng đẹp'
+  * @param content 'params0'
+  * @param type = "log" 'params1'
+  */
 	function boxAlert(content, type = "log") {
 		switch (type) {
 			case "log":
@@ -51,6 +64,10 @@
 	}
 
 	// Kiểm tra phiên bản
+ /**
+  * @func check_version
+  * @description 'Kiểm tra phiên bản hiện tại với phiên bản trên server'
+  */
 	var check_version = () => {
 		boxAlert(`Đang kiểm tra phiên bản...`, "log");
 		getUrlServer().then(latest_version => {
@@ -63,8 +80,33 @@
 
 	check_version();
 
+ /**
+  * @func INIT_CONFIG
+  * @description 'Khởi tạo cấu hình chương trình'
+  */
 	function INIT_CONFIG(){
 		// Khởi tạo cấu hình
+	}
+
+ /**
+  * @func INIT_UI
+  * @description 'Khởi tạo giao diện chương trình'
+  */
+	function INIT_UI(){
+		// Khởi tạo giao diện
+	}
+
+ /**
+  * @func INIT
+  * @description 'Khởi tạo chương trình'
+  */
+	function INIT(){
+		// Khởi tạo cấu hình
+		INIT_CONFIG();
+
+		// Khởi tạo giao diện
+		INIT_UI();
+
 	}
 
 })();
