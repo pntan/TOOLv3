eac50fab55077e224f233c2a6792e2b4974528f0
Tân <nhattanphan2014@gmail.com>
2025-11-07 15:55:07 +0700

    Create TOOL.js

---
+++ TOOL.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         CÔNG CỤ HỖ TRỢ V3
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=http://anonymouse.org/
+// @namespace    tnaphan.toolv3
+// @version      0.0.1
+// @description  Một số công cụ hỗ trợ công việc
+// @author       Phan Nhật Tân
+// @match        *://*/*
+// @grant        none
+// @updateURL    https://cdn.jsdelivr.net/gh/pntan/TOOLv3@lastest/tool.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/pntan/TOOLv3@lastest/tool.js
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @require      https://code.jquery.com/ui/1.14.1/jquery-ui.js
+// @require      https://cdn.jsdelivr.net/npm/dompurify@2.4.0/dist/purify.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Code của bạn
+})();
